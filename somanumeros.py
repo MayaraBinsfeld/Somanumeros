016b8736332ac2be42b158ef07e255d4d6912f28
MayaraBinsfeld <mayara.binsfeld@gmail.com>
2025-02-25 20:39:44 -0300

    Add files via upload

---
+++ somanumeros.py
@@ -0,0 +1,27 @@
+Descrição:
+Este programa soma dois números escolhidos pelo usuário
+Autor: Mayara de Almeida Binsfeld
+Versão: 0.0.1
+Data: 25/02/2025
+"""
+# Alocação de memória 
+
+numero_1 = 0
+
+numero_2 = 0
+
+resultado = 0
+
+# Entrada de dados
+numero_1 = int(input ("Escolha um número"))
+
+numero_2 = int(input ("Escolha outro número"))
+
+
+# Processamento de dados
+
+resultado = numero_1 + numero_2
+
+# Saida de dados
+
+print(f"A soma do {numero_1} com o {numero_2} é igual a {resultado}.")
